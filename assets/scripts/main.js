6647d3beef95f99302ea4936e3db7056f1af44d5
DylanMoriarty <moriartymaps@gmail.com>
2023-11-18 17:29:24 +0000

    Update master to output generated at 4fbc66c

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,17 +3,6 @@ document.addEventListener("DOMContentLoaded", function(e) {
 	const chapters = document.querySelectorAll('.Chapterbox')
 	const isArticle = document.querySelector('.article-content')
 
-	// if(postsParent) {
-	// 	Array.prototype.forEach.call(postsParent.children, child => {
-	// 		let serendipity = Math.random() * 120 + 'px'
-
-	// 		console.log(serendipity)
-
-	// 		child.style.marginRight = serendipity
-	// 		child.style.marginLeft = serendipity
-	// 	});
-	// }
-
 	if(chapters) {
 		document.addEventListener('scroll', function(e) {
 			for (var i = chapters.length - 1; i >= 0; i--) {
@@ -31,6 +20,3 @@ document.addEventListener("DOMContentLoaded", function(e) {
 })
 
 var prevScrollpos = window.pageYOffset;
-
-
-
